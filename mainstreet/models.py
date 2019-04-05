d0e519ec78b65f473b30f2f0fc9fe4a1eda00288
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-04-05 15:19:18 -0400

    added more heroku settings

--- mainstreet/models.py
+++ mainstreet/models.py
@@ -1,7 +1,16 @@
 # from django.db import models
 #
 #
-# class Items(models.Model):
+# class Instruments(models.Model):
+#     type = models.CharField(max_length=250)
+#     title = models.CharField(max_length=250)
+#     description = models.TextField(max_length=2000)
+#     price = models.DecimalField(max_digits=6, de)
+#     image = models.ImageField()
+#     video = models.FileField()
+#
+#
+# class Equipment(models.Model):
 #     type = models.CharField(max_length=250)
 #     title = models.CharField(max_length=250)
 #     description = models.TextField(max_length=2000)
@@ -9,14 +18,3 @@
 #     image = models.ImageField()
 #     video = models.FileField()
 
-
-# class Image(models.Model):
-#     post = models.ForeignKey(Items, related_name='images')
-#     file = models.ImageField(upload_to='upload')
-#     position = models.PositiveSmallIntegerField(default=0)
-#
-#     class Meta:
-#         ordering = ['position']
-#
-#     def __str__(self):
-#         return '%s - %s ' % (self.post, self.file)
